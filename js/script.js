8610ac3611165d46c8452b8d535ac4ef2b91322d
bestel89 <ben.stell1989@gmail.com>
2023-05-05 17:15:29 +0100

    responsive, hover details

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
+/*----- constants -----*/
+const btnLookup = {
+    red: {hoverClr: '#eb4723'},
+    grn: {hoverClr: '#1aad49'},
+    yel: {hoverClr: '#faed34'},
+    blu: {hoverClr: '#169ae0'},
+  };
+
 /*----- app's state (variables) -----*/
 
 
 /*----- cached element references -----*/
-
+document.getElementById('board').addEventListener('click', handleClick)
 
 /*----- classes -----*/
 
 
 /*----- functions -----*/
-
+function handleClick(evt) {
+    // //guards
+    if (evt.target.classList.value !== 'clrbtns') return;
+    // // console.log(evt.target.classList.value)
+    let clickedClr = btnLookup[evt.target.id].hoverClr;
+    document.getElementById(`${evt.target.id}`).style.backgroundColor = clickedClr;
+};
 
 /*----- eventListeners -----*/
