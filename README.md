c6c2c64140ff93398d25ece17a58c1ac17c148d4
bestel89 <ben.stell1989@gmail.com>
2023-05-06 17:10:36 +0100

    fixed hover, onclick and sounds

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # simongame
+
+Sound Effect by <a href="https://pixabay.com/users/universfield-28281460/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=124464">UNIVERSFIELD</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=124464">Pixabay</a>
--- js/script.js
+++ js/script.js
@@ -1,27 +1,67 @@
 /*----- constants -----*/
 const btnLookup = {
-    red: {hoverClr: '#eb4723'},
-    grn: {hoverClr: '#1aad49'},
-    yel: {hoverClr: '#faed34'},
-    blu: {hoverClr: '#169ae0'},
+    red: {clickedClr: '--redL'},
+    grn: {clickedClr: '--greenL'},
+    yel: {clickedClr: '--yellowL'},
+    blu: {clickedClr: '--blueL'},
   };
+
+const sounds = {
+    red: 'sounds/btn1.mp3',
+    grn: 'sounds/btn2.mp3',
+    yel: 'sounds/btn3.mp3',
+    blu: 'sounds/btn4.mp3',
+    gst: '',
+    gov: '',
+}
 
 /*----- app's state (variables) -----*/
 
 
 /*----- cached element references -----*/
-document.getElementById('board').addEventListener('click', handleClick)
+const boardEl = document.getElementById('board')
+const redEl = document.getElementById('red');
+const grnEl = document.getElementById('grn');
+const yelEl = document.getElementById('yel');
+const bluEl = document.getElementById('blu');
+const audioPlayer = new Audio();
+
+//Audio controls:
+// audioPlayer.volume = .5;
 
 /*----- classes -----*/
 
 
 /*----- functions -----*/
 function handleClick(evt) {
-    // //guards
+    //guards to prevent improper clicking
     if (evt.target.classList.value !== 'clrbtns') return;
-    // // console.log(evt.target.classList.value)
-    let clickedClr = btnLookup[evt.target.id].hoverClr;
-    document.getElementById(`${evt.target.id}`).style.backgroundColor = clickedClr;
+    //else if button is actually clicked...
+    handleSound(evt);
+    renderBtnClr(evt);
 };
 
+function renderBtnClr(evt) {
+    //get the current clicked button and change its ID to make the styling brighter
+    let clickedBtn = document.getElementById(`${evt.target.id}`)
+    clickedBtn.id = `${evt.target.id}Clicked`;
+    //set a timeout to change ID back to original after a timer
+    setTimeout(() => {
+        const trimmedId = clickedBtn.id.slice(0, 3);
+        clickedBtn.id = trimmedId;
+    }, 500);
+}
+
+function handleSound(evt) {
+    const clickedBtn = document.getElementById(`${evt.target.id}`)
+    console.log(sounds[clickedBtn.id]);
+    playSound(clickedBtn.id);
+}
+
+function playSound(name) {
+    audioPlayer.src = sounds[name];
+    audioPlayer.play();
+}
+
 /*----- eventListeners -----*/
+boardEl.addEventListener('mousedown', handleClick)
